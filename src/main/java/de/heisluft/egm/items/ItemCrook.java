aed61d1c9e5e5d8aab7243cad66dc5be16a931dc
BuildTools <unconfigured@null.spigotmc.org>
2017-08-25 10:52:51 +0200

    Updating to the more appropiate GPLv3 License

--- src/main/java/de/heisluft/egm/items/ItemCrook.java
+++ src/main/java/de/heisluft/egm/items/ItemCrook.java
@@ -19,37 +19,38 @@ import net.minecraft.world.World;
 public class ItemCrook extends ItemTool {
 	public static enum Variation {
 		WOOD("wooden_crook", ToolMaterial.WOOD), BONE("bone_crook", ToolMaterial.STONE);
-
+		
 		private final String name;
 		private final ToolMaterial material;
-
+		
 		private Variation(String name, ToolMaterial material) {
 			this.material = material;
 			this.name = name;
 		}
-
+		
 		public ToolMaterial getMaterial() {
 			return material;
 		}
-
+		
 		public String getName() {
 			return name;
 		}
 	}
-
+	
 	private static final Set<Block> effectiveBlocks = Sets.<Block>newHashSet();
-
+	
 	{
 		effectiveBlocks.add(Blocks.LEAVES);
 		effectiveBlocks.add(Blocks.LEAVES2);
 	}
-
+	
 	public ItemCrook(Variation var) {
 		super(var.getMaterial(), effectiveBlocks);
 		setUnlocalizedName(var.getName());
 		setRegistryName(var.getName());
+		setCreativeTab(null);
 	}
-	
+
 	@Override
 	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos,
 			EntityLivingBase entityLiving) {
